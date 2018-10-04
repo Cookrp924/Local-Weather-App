ab02b5625996af6894e6813de9dbc52f774df5e1
Cookrp924 <34467782+Cookrp924@users.noreply.github.com>
2018-10-03 22:19:53 -0400

    Update LocalWeather.js

--- LocalWeather.js
+++ LocalWeather.js
@@ -1,25 +1,33 @@
 function getWeather(lat, lon){
 	$(document).ready(function(){
+		//JSON-encoded data from server via a GET HTTP request
+		//OpenWeatherMap API
 		$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b7fc987f11d7a2fe55fa4907398cfada", function(data){
-  				$("#city").html(data.name + ", NC");
+  				//display selected data
+				$("#city").html(data.name + ", NC");
   				$("#weather").html(data.weather[0].description);
   				$("#humidity").html(data.main.humidity + " %");
   				
+				//display Far & Cel
   				var Far = Math.round([data.main.temp * (9/5)] - 459.67);
   				var Cel = Math.round(data.main.temp - 273.15);
   				$("#temp").html(Far + " ºF");
-  				$("#temp").click(function(){
+  				
+				//click between Far & Cel
+				$("#temp").click(function(){
   					var text = $("#temp").text();
   					$(this).text(text == Cel + " ºC" ? Far + " ºF" : Cel + " ºC");
   				});
 
-  				var iconCode = data.weather[0].icon;
+  				//display weather icon
+				var iconCode = data.weather[0].icon;
   				var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
   				$("#icon").html("<img src='" + iconURL + "'>");
   		})
 	})
 };
 
+//geolocation API for latitude & longitude
 function getLocation(){
 	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(function(position){
